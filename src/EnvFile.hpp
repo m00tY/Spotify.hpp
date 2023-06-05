d881d19ee36d147fee1f2b1372d013dde4b2bdf5
Zac <zacrex@outlook.com>
2023-06-05 18:54:30 -0400

    Initial commit

---
+++ src/EnvFile.hpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+class EnvFile {
+    private:
+    std::ifstream file;
+    
+    string filePath;
+    map<string, string> envContents;
+
+    map<string, string> envToMap(string filePath) {
+        file.open(filePath);
+        if (!file) {
+            throw "could not open file";
+        }
+
+        string line;
+        map<string, string> fileContents;
+        while (file) {
+            getline(file, line);
+            auto linePair = breakAtEquals(line);
+            fileContents[linePair.first] = linePair.second;
+        }
+        return fileContents;
+    }
+    
+    //input should only be one line, while loop is jank but works
+    std::pair<string, string> breakAtEquals(string text) {
+        string key;
+        string value;
+        
+        string delimiter = "=";
+        int pos = 0;
+        while ((pos = text.find(delimiter)) != string::npos) {
+            key = text.substr(0, pos);
+            value = text.substr(pos+1, text.length());
+        }
+        return std::make_pair(key, value);
+    }
+    
+    public:
+    EnvFile(string filePath) {
+        this->filePath = filePath;
+        envContents = envToMap(filePath); 
+    }
+
+    map<string, string> getMap() {
+        return envContents;
+    }
+    
+};
